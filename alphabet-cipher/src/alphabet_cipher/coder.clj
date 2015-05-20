e870f1c29b71860cbeae732ec70144d767555f55
robhawkins <stillastudent@gmail.com>
2015-05-20 20:59:54 +0800

    my first clojure program

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,29 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+; my first clojure program.
+; hopefully i will get more comfortable using 'map' instead of 'for' in order to loop
+; i was stuck on 'for' because my first idea was to use an equation that uses an index
 
+
+(defn char-int [c]
+  (- (int c) 97))
+(defn int-char [i]
+  (char (+ i 97)))
+
+; encoded[i] = (keyword[i % keyword-length] + message[i]) % 26
+(defn encode [keyword message]
+  (apply str (for [i (range 0 (count message))
+        :let [m (int-char (mod (+ (char-int (nth keyword (mod i (count keyword))))
+                                  (char-int (nth message i)))
+                               26))]]
+    m)))
+
+; decoded[i] = (message[i] - keyword[i % keyword-length]) % 26
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (for [i (range 0 (count message))
+        :let [m (int-char (mod (- (char-int (nth message i))
+                                  (char-int (nth keyword (mod i (count keyword)))))
+
+                               26))]]
+    m)))
+
